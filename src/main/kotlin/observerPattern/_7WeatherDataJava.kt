4732edecba1cdcf56f1720cfdd55cb869c46e0f5
brent <jinhoshin319@gmail.com>
2021-05-11 10:46:17 +0900

    Subject extending built-in Observable

---
+++ src/main/kotlin/observerPattern/_7WeatherDataJava.kt
@@ -0,0 +1,28 @@
+package observerPattern
+
+import java.util.*
+
+class _7WeatherDataJava : Observable() {
+
+    private var temperature = 0f
+    private var humidity = 0f
+    private var pressure = 0f
+
+    private fun measurementsChanged() {
+        setChanged()
+        notifyObservers()
+    }
+
+    fun setMeasurements(temperature: Float, humidity: Float, pressure: Float) {
+        this.temperature = temperature
+        this.humidity = humidity
+        this.pressure = pressure
+        measurementsChanged()
+    }
+
+    fun getTemperature() = temperature
+    fun getHumidity() = humidity
+    fun getPressure() = pressure
+
+
+}
