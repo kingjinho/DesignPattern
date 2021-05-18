a86750d1c8faeb5d7a261d88ead663641cc000aa
brent <jinhoshin319@gmail.com>
2021-05-18 11:01:21 +0900

    adapter that alters Iterator to Enumerator

---
+++ src/main/kotlin/adapterPattern/EnumeratorIterator.kt
@@ -0,0 +1,20 @@
+package adapterPattern
+
+import java.lang.UnsupportedOperationException
+import java.util.*
+
+
+class EnumeratorIterator(private val enum: Enumeration<Int>) : Iterator<Int> {
+
+    override fun hasNext(): Boolean {
+        return enum.hasMoreElements()
+    }
+
+    override fun next(): Int {
+        return enum.nextElement()
+    }
+
+    fun remove() {
+        throw UnsupportedOperationException()
+    }
+}
