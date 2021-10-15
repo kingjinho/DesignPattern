9c118544c5d5a4a83f7f1d60a656017daa0ef801
brent <jinhoshin319@gmail.com>
2021-10-15 13:02:32 +0900

    misc

--- src/main/kotlin/decorator/Shoe.kt
+++ src/main/kotlin/decorator/Shoe.kt
@@ -4,11 +4,11 @@ abstract class Shoe {
 
     private var description = "Unknown Shoes"
 
-    abstract fun cost(): Int
-
     fun setDescription(description: String) {
         this.description = description
     }
+
+    abstract fun cost(): Int
 
     open fun getDescription() = description
 
